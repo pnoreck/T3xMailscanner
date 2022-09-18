c9574668285f8f1bbeb2b9e38f72d90216013fb6
pnoreck <github@t3x.ch>
2022-09-18 23:48:09 +0200

    * Refactoring for TYPO3 V11

--- packages/t3fx_mailscanner/Configuration/TCA/tx_t3fxmailscanner_domain_model_imapfolder.php
+++ packages/t3fx_mailscanner/Configuration/TCA/tx_t3fxmailscanner_domain_model_imapfolder.php
@@ -1,90 +1,58 @@
 <?php
 return array(
-	'ctrl' => array(
-		'title'	=> 'LLL:EXT:t3fx_mailscanner/Resources/Private/Language/locallang_db.xlf:tx_t3fxmailscanner_domain_model_imapfolder',
-		'label' => 'name',
-		'tstamp' => 'tstamp',
-		'crdate' => 'crdate',
-		'cruser_id' => 'cruser_id',
-		'dividers2tabs' => TRUE,
-		'delete' => 'deleted',
-		'enablecolumns' => array(
-			'disabled' => 'hidden',
-			'starttime' => 'starttime',
-			'endtime' => 'endtime',
-		),
-		'searchFields' => 'full_name,name',
-		'iconfile' => 'EXT:t3fx_mailscanner/Resources/Public/Icons/tx_t3fxmailscanner_domain_model_imapfolder.gif'
-	),
-	'interface' => array(
-		'showRecordFieldList' => 'hidden, full_name, name',
-	),
-	'types' => array(
-		'1' => array('showitem' => 'hidden;;1, full_name, name, --div--;LLL:EXT:cms/locallang_ttc.xlf:tabs.access, starttime, endtime'),
-	),
-	'palettes' => array(
-		'1' => array('showitem' => ''),
-	),
-	'columns' => array(
+    'ctrl'    => array(
+        'title'            => 'LLL:EXT:t3fx_mailscanner/Resources/Private/Language/locallang_db.xlf:tx_t3fxmailscanner_domain_model_imapfolder',
+        'label'            => 'name',
+        'tstamp'           => 'tstamp',
+        'crdate'           => 'crdate',
+        'default_sortby'   => 'ORDER BY full_name',
+        'cruser_id'        => 'cruser_id',
+        'delete'           => 'deleted',
+        'enablecolumns'    => array(
+            'disabled' => 'hidden',
+        ),
+        'typeicon_classes' => [
+            'default' => 'tx_examples-dummy',
+        ],
+    ),
+    'types'   => array(
+        '0' => array('showitem' => 'hidden, full_name, name'),
+    ),
+    'columns' => array(
 
-		'hidden' => array(
-			'exclude' => 1,
-			'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.hidden',
-			'config' => array(
-				'type' => 'check',
-			),
-		),
-		'starttime' => array(
-			'exclude' => 1,
-			'l10n_mode' => 'mergeIfNotBlank',
-			'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.starttime',
-			'config' => array(
-				'type' => 'input',
-				'size' => 13,
-				'max' => 20,
-				'eval' => 'datetime',
-				'checkbox' => 0,
-				'default' => 0,
-				'range' => array(
-					'lower' => mktime(0, 0, 0, date('m'), date('d'), date('Y'))
-				),
-			),
-		),
-		'endtime' => array(
-			'exclude' => 1,
-			'l10n_mode' => 'mergeIfNotBlank',
-			'label' => 'LLL:EXT:lang/locallang_general.xlf:LGL.endtime',
-			'config' => array(
-				'type' => 'input',
-				'size' => 13,
-				'max' => 20,
-				'eval' => 'datetime',
-				'checkbox' => 0,
-				'default' => 0,
-				'range' => array(
-					'lower' => mktime(0, 0, 0, date('m'), date('d'), date('Y'))
-				),
-			),
-		),
+        'hidden' => [
+            'exclude' => true,
+            'label'   => 'LLL:EXT:core/Resources/Private/Language/locallang_general.xlf:LGL.enabled',
+            'config'  => [
+                'type'       => 'check',
+                'renderType' => 'checkboxToggle',
+                'items'      => [
+                    [
+                        0                    => '',
+                        1                    => '',
+                        'invertStateDisplay' => true
+                    ]
+                ],
+            ]
+        ],
 
-		'full_name' => array(
-			'exclude' => 1,
-			'label' => 'LLL:EXT:t3fx_mailscanner/Resources/Private/Language/locallang_db.xlf:tx_t3fxmailscanner_domain_model_imapfolder.full_name',
-			'config' => array(
-				'type' => 'input',
-				'size' => 30,
-				'eval' => 'trim'
-			),
-		),
-		'name' => array(
-			'exclude' => 1,
-			'label' => 'LLL:EXT:t3fx_mailscanner/Resources/Private/Language/locallang_db.xlf:tx_t3fxmailscanner_domain_model_imapfolder.name',
-			'config' => array(
-				'type' => 'input',
-				'size' => 30,
-				'eval' => 'trim'
-			),
-		),
-
-	),
+        'full_name' => array(
+            'exclude' => 1,
+            'label'   => 'LLL:EXT:t3fx_mailscanner/Resources/Private/Language/locallang_db.xlf:tx_t3fxmailscanner_domain_model_imapfolder.full_name',
+            'config'  => array(
+                'type' => 'input',
+                'size' => 30,
+                'eval' => 'trim'
+            ),
+        ),
+        'name'      => array(
+            'exclude' => 1,
+            'label'   => 'LLL:EXT:t3fx_mailscanner/Resources/Private/Language/locallang_db.xlf:tx_t3fxmailscanner_domain_model_imapfolder.name',
+            'config'  => array(
+                'type' => 'input',
+                'size' => 30,
+                'eval' => 'trim'
+            ),
+        ),
+    ),
 );
