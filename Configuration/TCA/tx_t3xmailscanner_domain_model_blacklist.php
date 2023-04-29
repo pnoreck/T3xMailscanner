e37bb2c784530312512a5ebf9cc4091049fe2307
pnoreck <github@t3x.ch>
2023-04-29 14:50:13 +0200

    Move TCA from T3fx to T3x

--- Configuration/TCA/tx_t3xmailscanner_domain_model_blacklist.php
+++ Configuration/TCA/tx_t3xmailscanner_domain_model_blacklist.php
@@ -60,6 +60,5 @@ return array(
                 'type' => 'check',
             ),
         ),
-
     ),
 );
--- ext_tables.sql
+++ ext_tables.sql
@@ -5,9 +5,9 @@
 --
 
 CREATE TABLE `tx_t3xmailscanner_domain_model_blacklist` (
-    `mail`            varchar(255)      NOT NULL DEFAULT '',
-    `domain`          varchar(255)      NOT NULL DEFAULT '',
-    `complete_domain` smallint UNSIGNED NOT NULL DEFAULT '0'
+    `mail`            varchar(255) NOT NULL DEFAULT '',
+    `domain`          varchar(255) NOT NULL DEFAULT '',
+    `complete_domain` smallint(5) unsigned NOT NULL DEFAULT 0,
 );
 
 -- --------------------------------------------------------
@@ -17,10 +17,10 @@ CREATE TABLE `tx_t3xmailscanner_domain_model_blacklist` (
 --
 
 CREATE TABLE `tx_t3xmailscanner_domain_model_content_filter` (
-    `sha1`        varchar(40)                                DEFAULT NULL,
-    `regex`       varchar(100)                      NOT NULL,
-    `content`     varchar(255)                      NOT NULL DEFAULT '',
-    `filter_type` enum ('SUBJECT','BODY','BOTH','') NOT NULL
+    `sha1`        varchar(40)  NOT NULL DEFAULT '',
+    `regex`       varchar(100) NOT NULL DEFAULT '',
+    `content`     varchar(255) NOT NULL DEFAULT '',
+    `filter_type` enum ('SUBJECT','BODY','BOTH','') NOT NULL DEFAULT ''
 );
 
 -- --------------------------------------------------------
@@ -42,13 +42,13 @@ CREATE TABLE `tx_t3xmailscanner_domain_model_imapfolder` (
 --
 
 CREATE TABLE `tx_t3xmailscanner_domain_model_mail` (
-    `from_name` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-    `from_mail` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-    `to_name`   varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-    `to_mail`   varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-    `subject`   varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL DEFAULT '',
-    `header`    text CHARACTER SET utf8 COLLATE utf8_unicode_ci,
-    `message`   text CHARACTER SET utf8 COLLATE utf8_unicode_ci
+    `from_name` varchar(255) NOT NULL DEFAULT '',
+    `from_mail` varchar(255) NOT NULL DEFAULT '',
+    `to_name`   varchar(255) NOT NULL DEFAULT '',
+    `to_mail`   varchar(255) NOT NULL DEFAULT '',
+    `subject`   varchar(255) NOT NULL DEFAULT '',
+    `header`    text                  DEFAULT NULL,
+    `message`   text                  DEFAULT NULL,
 );
 
 -- --------------------------------------------------------
@@ -58,19 +58,19 @@ CREATE TABLE `tx_t3xmailscanner_domain_model_mail` (
 --
 
 CREATE TABLE `tx_t3xmailscanner_domain_model_sender` (
-    `uid`         int               NOT NULL,
-    `pid`         int               NOT NULL DEFAULT '0',
-    `name`        varchar(255)      NOT NULL DEFAULT '',
-    `imap_folder` int UNSIGNED               DEFAULT '0',
-    `tstamp`      int UNSIGNED      NOT NULL DEFAULT '0',
-    `crdate`      int UNSIGNED      NOT NULL DEFAULT '0',
-    `cruser_id`   int UNSIGNED      NOT NULL DEFAULT '0',
+    `uid`         int          NOT NULL,
+    `pid`         int          NOT NULL DEFAULT '0',
+    `name`        varchar(255) NOT NULL DEFAULT '',
+    `imap_folder` int UNSIGNED DEFAULT '0',
+    `tstamp`      int UNSIGNED NOT NULL DEFAULT '0',
+    `crdate`      int UNSIGNED NOT NULL DEFAULT '0',
+    `cruser_id`   int UNSIGNED NOT NULL DEFAULT '0',
     `deleted`     smallint UNSIGNED NOT NULL DEFAULT '0',
     `hidden`      smallint UNSIGNED NOT NULL DEFAULT '0',
-    `starttime`   int UNSIGNED      NOT NULL DEFAULT '0',
-    `endtime`     int UNSIGNED      NOT NULL DEFAULT '0',
+    `starttime`   int UNSIGNED NOT NULL DEFAULT '0',
+    `endtime`     int UNSIGNED NOT NULL DEFAULT '0',
 
-    KEY `name`(`name`, `deleted`, `hidden`) USING BTREE
+    KEY           `name`(`name`, `deleted`, `hidden`) USING BTREE
 );
 
 -- --------------------------------------------------------
@@ -80,17 +80,26 @@ CREATE TABLE `tx_t3xmailscanner_domain_model_sender` (
 --
 
 CREATE TABLE `tx_t3xmailscanner_domain_model_whitelist` (
-    `uid`       int              NOT NULL,
-    `pid`       int              NOT NULL DEFAULT '0',
-    `name`      varchar(255)     NOT NULL DEFAULT '',
-    `tstamp`    int UNSIGNED     NOT NULL DEFAULT '0',
-    `crdate`    int UNSIGNED     NOT NULL DEFAULT '0',
-    `cruser_id` int UNSIGNED     NOT NULL DEFAULT '0',
+    `uid`       int          NOT NULL,
+    `pid`       int          NOT NULL DEFAULT '0',
+    `name`      varchar(255) NOT NULL DEFAULT '',
+    `tstamp`    int UNSIGNED NOT NULL DEFAULT '0',
+    `crdate`    int UNSIGNED NOT NULL DEFAULT '0',
+    `cruser_id` int UNSIGNED NOT NULL DEFAULT '0',
     `deleted`   tinyint UNSIGNED NOT NULL DEFAULT '0',
     `hidden`    tinyint UNSIGNED NOT NULL DEFAULT '0',
-    `starttime` int UNSIGNED     NOT NULL DEFAULT '0',
-    `endtime`   int UNSIGNED     NOT NULL DEFAULT '0',
+    `starttime` int UNSIGNED NOT NULL DEFAULT '0',
+    `endtime`   int UNSIGNED NOT NULL DEFAULT '0',
 
-    KEY `name`(`name`, `deleted`, `hidden`) USING BTREE
+    KEY         `name`(`name`, `deleted`, `hidden`) USING BTREE
 );
 
+
+CREATE TABLE `t3fx_weather` (
+  `uid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `crdate` int(11) NOT NULL,
+  `tstamp` int(11) NOT NULL,
+  `city_id` int(11) NOT NULL,
+  `json` text NOT NULL,
+  PRIMARY KEY (`uid`)
+);
